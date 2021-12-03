ab55f8699325d2975dd0688c3fade65a2f2a5674
AnnesBlursedCreations <95392649+AnnesBlursedCreations@users.noreply.github.com>
2021-12-03 14:34:39 -0800

    Add files via upload

--- script.js
+++ script.js
@@ -1,33 +1,59 @@
-const file = 'data.csv';
-const character_headers = ['adjective', 'race', 'character_class', 'origin', 'who'];
-const item_headers = ['sentimental_magic_item_d100', 'magical_item'];
-const setting_headers = ['atmosphere', 'primary', 'secondary', 'ground', 'water'];
+/**
+ * Random D&D Generator Script
+ */
 
+//Data file path (relative).
+const file = "data.csv";
+
+// Headers.
+const character_headers = ["adjective", "race", "character_class", "origin", "who"];
+const item_headers = ["sentimental_magic_item_d100", "magical_item"];
+const setting_headers = ["atmosphere", "primary", "secondary", "ground", "water"];
+
+// Global csv data dictionary.
 var csv_data = {};
 
 
+function init() {
+    /**
+     * Initialize the document.
+     */
+    load_file(csv_parser);
+}
+
+
 function load_file(callback) {
+    /**
+     * Load the csv file and run the callback function.
+     */
     var reader = new XMLHttpRequest();
 
+    // Async file reader.
     reader.onreadystatechange = function () {
         if (reader.readyState === 4 && reader.status === 200 && callback) callback(reader.responseText);
     }
-    reader.open('get', file, true);
+    reader.open("get", file, true);
     reader.send();
 }
 
 
 function csv_parser(raw_csv_data) {
+    /**
+     * Take the raw csv data and convert it to a dictionary.
+     */
+    // Use jquery-csv to convert the raw csv data to a 2D array.
     let rows = $.csv.toArrays(raw_csv_data);
     let headers = rows[0];
 
+    // Convert the 2D array to a dictionary of header: [col1, col2, ...]
+    // essentially, converting the rows to columns.
     for (let h = 0; h < headers.length; h++) {
-        let header = headers[h].replace(/\s/gi, '_').toLowerCase();
+        let header = headers[h].replace(/\s/gi, "_").toLowerCase();
         csv_data[header] = [];
 
         for (let r = 1; r < rows.length; r++) {
             let row_data = rows[r];
-            if (row_data[h] != undefined && row_data[h] != '') {
+            if (row_data[h] != undefined && row_data[h] != "") {
                 csv_data[header].push(row_data[h].toLowerCase());
             }
         }
@@ -36,6 +62,9 @@ function csv_parser(raw_csv_data) {
 
 
 function get_attributes(headers) {
+    /**
+     * Pick a random value from the csv dictionary for each given header.
+     */
     let attr_data = {};
     for (let h = 0; h < headers.length; h++) {
         let header = headers[h].toLowerCase();
@@ -49,28 +78,60 @@ function get_attributes(headers) {
 
 
 function create_character() {
-    let determiner = 'a'
+    /**
+     * Fill in the character data, show the character fields and hide
+     * everything else/
+     */
+    let determiner = "a"
+    document.getElementById("determiner").textContent = determiner;
+
     let character_data = get_attributes(character_headers);
-    let output = `<p>You are ${determiner} <span id="adjective">${character_data.adjective}</span> <span id="race">${to_title_case(character_data.race)}</span> <span id="character_class">${to_title_case(character_data.character_class)}</span> from <span id="origin">${character_data.origin}</span> who <span id="who">${character_data.who}</span>.</p>`;
-    document.getElementById("printer").innerHTML = output;
+    for (let h = 0; h < character_headers.length; h++) {
+        let header = character_headers[h]
+        document.getElementById(header).textContent = character_data[header];
+    }
+    $("#character").show();
+    $("#items").hide();
+    $("#setting").hide();
 }
 
 
 function create_items() {
+     /**
+     * Fill in the item data, show the item fields and hide
+     * everything else/
+     */
     let item_data = get_attributes(item_headers);
-    let output = `<p>You have <span id="sentimental_magic_item_d100">${item_data['sentimental_magic_item_d100']}</span><br /><br />and<br /><br /><span id="magical_item">${item_data['magical_item']}</span></p>`;
-    document.getElementById("printer").innerHTML = output;
+    for (let h = 0; h < item_headers.length; h++) {
+        let header = item_headers[h]
+        document.getElementById(header).textContent = item_data[header];
+    }
+    $("#character").hide();
+    $("#items").show();
+    $("#setting").hide();
 }
 
 
 function create_setting() {
+     /**
+     * Fill in the setting data, show the setting fields and hide
+     * everything else/
+     */
     let setting_data = get_attributes(setting_headers);
-    let output = `<p>You are in a <span id="atmosphere">${setting_data.atmosphere}</span> place with <span id="primary">${setting_data.primary}</span> and <span id="secondary">${setting_data.secondary}</span>.</p>`;
-    document.getElementById("printer").innerHTML = output;
+    for (let h = 0; h < setting_headers.length; h++) {
+        let header = setting_headers[h]
+        document.getElementById(header).textContent = setting_data[header];
+    }
+    $("#character").hide();
+    $("#items").hide();
+    $("#setting").show();
 }
 
 
 function to_title_case(str) {
+    /**
+     * Convert a string to title case.
+     */
     return str.replace(
         /\w\S*/g,
         function(txt) {
@@ -80,4 +141,4 @@ function to_title_case(str) {
 }
 
 
-$('document').ready(load_file(csv_parser));
+$("document").ready(init());
